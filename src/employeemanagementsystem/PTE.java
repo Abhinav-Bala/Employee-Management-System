6d05b5e40a9cba42f9961d44b3fbe93b6a376bcb
Abhinav-Bala <abhinav.bala24@gmail.com>
2021-12-31 16:22:30 -0500

    First Commit

---
+++ src/employeemanagementsystem/PTE.java
@@ -0,0 +1,24 @@
+package employeemanagementsystem;
+
+
+   // PART TIME EMPLOYEE
+
+public class PTE extends EmployeeInfo {
+    
+    
+    public double hourlyWage;
+    public double hoursPerWeek;
+    public double weeksPerYear;
+    
+    
+    public PTE(int eN, String fN, String lN, String g, String wL, double dR, double hW, double hPW, double wPY) {
+        super(eN, fN, lN, g, wL, dR);
+        hourlyWage = hW;
+        hoursPerWeek = hPW;
+        weeksPerYear = wPY;
+        
+    }
+    
+    
+    
+}
